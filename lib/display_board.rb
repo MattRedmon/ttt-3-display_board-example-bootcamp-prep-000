b478799271cd743611a5a564a9dfed5a0bb91355
Matt Redmon <matt.redmon@live.com>
2018-01-21 00:59:53 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  arr = ["X"]
-  board = " #{arr[0]} |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n"
+  board = "  |   |   \n-----------\n   |   |   \n-----------\n   |   |   \n"
   puts board
 end
 display_board
